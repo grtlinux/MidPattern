79b40c41179dffc36909fa88de05f8f1dd9dbc31
grtlinux <grtlinux@gmail.com>
2018-09-06 18:27:24 +0900

    create App4.java

--- Html2Pdf_01/src/main/java/org/tain/html2pdf01/App4.java
+++ Html2Pdf_01/src/main/java/org/tain/html2pdf01/App4.java
@@ -1,19 +1,10 @@
 package org.tain.html2pdf01;
 
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.html.simpleparser.HTMLWorker;
-import com.itextpdf.text.html.simpleparser.StyleSheet;
-import com.itextpdf.text.pdf.PdfWriter;
 
-@SuppressWarnings("deprecation")
-public class App2 {
+public class App4 {
 
 	private static boolean flag;
 	private static String HTML_FILE;
@@ -44,20 +35,6 @@ public class App2 {
     }
 
 	private static void test01(String[] args) throws DocumentException, IOException {
-		Document document = new Document();
-		PdfWriter.getInstance(document, new FileOutputStream(PDF_FILE));
 
-		document.open();
-
-		StyleSheet styles = new StyleSheet();
-		styles.loadStyle("imdb", "size", "-e");
-
-		HTMLWorker htmlWorker = new HTMLWorker(document, null, styles);
-		Map<String, Object> providers = new HashMap<String, Object>();
-		//providers.put(HTMLWorker.IMG_PROVIDER, new MyImageFactory());
-		htmlWorker.setProviders(providers);
-		htmlWorker.parse(new FileReader(HTML_FILE));
-
-		document.close();
 	}
 }
