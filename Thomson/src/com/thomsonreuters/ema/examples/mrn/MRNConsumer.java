af42363c1a955820cb054bca10bddfa8932b83f8
grtlinux <grtlinux@gmail.com>
2018-09-10 16:16:03 +0900

    1st create

---
+++ Thomson/src/com/thomsonreuters/ema/examples/mrn/MRNConsumer.java
@@ -0,0 +1,295 @@
+package com.thomsonreuters.ema.examples.mrn;
+
+import com.thomsonreuters.ema.access.AckMsg;
+import com.thomsonreuters.ema.access.Data;
+import com.thomsonreuters.ema.access.GenericMsg;
+import com.thomsonreuters.ema.access.RefreshMsg;
+import com.thomsonreuters.ema.access.ReqMsg;
+import com.thomsonreuters.ema.access.StatusMsg;
+import com.thomsonreuters.ema.access.UpdateMsg;
+import com.thomsonreuters.ema.access.DataType;
+import com.thomsonreuters.ema.access.DataType.DataTypes;
+import com.thomsonreuters.ema.access.EmaFactory;
+import com.thomsonreuters.ema.access.EmaUtility;
+import com.thomsonreuters.ema.access.FieldEntry;
+import com.thomsonreuters.ema.access.FieldList;
+import com.thomsonreuters.ema.access.Map;
+import com.thomsonreuters.ema.access.MapEntry;
+import com.thomsonreuters.ema.access.OmmConsumer;
+import com.thomsonreuters.ema.access.OmmConsumerClient;
+import com.thomsonreuters.ema.access.OmmConsumerConfig;
+import com.thomsonreuters.ema.access.OmmConsumerEvent;
+import com.thomsonreuters.ema.access.OmmException;
+import com.thomsonreuters.ema.rdm.EmaRdm;
+import org.json.JSONObject;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Iterator;
+import java.util.zip.GZIPInputStream;
+import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.thomsonreuters.ema.access.Msg;
+import java.nio.ByteBuffer;
+
+class AppClient implements OmmConsumerClient {
+
+    public static final int FRAGMENT =  32641;
+    public static final int TOT_SIZE =  32480;
+    public static final int GUID =      4271;
+    
+    // store fragments for reassmbly
+    Hashtable<String,ArrayList<ByteBuffer>> fragBuilderHash = 
+            new Hashtable<String,ArrayList<ByteBuffer>>();
+    // store total sizes
+    Hashtable<String,Long> totalSizes = new Hashtable<String,Long>();
+    private MRNFXMain _mn = null;
+    
+    public AppClient() {}
+    
+    public void setFXMain(MRNFXMain mn) {
+        _mn = mn;
+    }
+
+    public void onRefreshMsg(RefreshMsg refreshMsg, OmmConsumerEvent event) {
+        System.out.println("onRefreshMsg");
+        System.out.println("Item Name: " + (refreshMsg.hasName() ? refreshMsg.name() : "<not set>"));
+        System.out.println("Service Name: " + (refreshMsg.hasServiceName() ? refreshMsg.serviceName() : "<not set>"));
+
+        System.out.println("Item State: " + refreshMsg.state());
+
+        if (DataType.DataTypes.MAP == refreshMsg.payload().dataType()) {
+            decode(refreshMsg.payload().map());
+        }
+
+        System.out.println();
+    }
+
+    public void onUpdateMsg(UpdateMsg updateMsg, OmmConsumerEvent event) {
+        System.out.println("onUpdateMsg");
+        System.out.println("Item Name: " + (updateMsg.hasName() ? updateMsg.name() : "<not set>"));
+        System.out.println("Service Name: " + (updateMsg.hasServiceName() ? updateMsg.serviceName() : "<not set>"));
+        System.out.println("Closure: "+event.closure());
+        
+        if (DataType.DataTypes.MAP == updateMsg.payload().dataType()) {
+            decode(updateMsg.payload().map());
+        } else if (DataType.DataTypes.FIELD_LIST == updateMsg.payload().dataType()) {
+            decode(updateMsg.payload().fieldList(),event.closure());
+        }
+
+        System.out.println();
+    }
+
+    public void onStatusMsg(StatusMsg statusMsg, OmmConsumerEvent event) {
+        System.out.println("Item Name: " + (statusMsg.hasName() ? statusMsg.name() : "<not set>"));
+        System.out.println("Service Name: " + (statusMsg.hasServiceName() ? statusMsg.serviceName() : "<not set>"));
+
+        if (statusMsg.hasState()) {
+            System.out.println("Item State: " + statusMsg.state());
+        }
+
+        System.out.println();
+    }
+
+    public void onGenericMsg(GenericMsg genericMsg, OmmConsumerEvent consumerEvent) {
+    }
+
+    public void onAckMsg(AckMsg ackMsg, OmmConsumerEvent consumerEvent) {
+    }
+
+    public void onAllMsg(Msg msg, OmmConsumerEvent consumerEvent) {
+    }
+
+    void decode(FieldList fieldList, Object closure) {
+        boolean retVal;
+        System.out.println("Decode fieldList:");
+        Iterator<FieldEntry> iter = fieldList.iterator();
+        FieldEntry fieldEntry;
+        long totalSize = 0;
+        String guid = "";
+        while (iter.hasNext()) {
+            fieldEntry = iter.next();
+            System.out.println("FIELD_ENTRY: " + fieldEntry.fieldId() + "/" + fieldEntry.name() + ": " + fieldEntry.load() + " loadType: " + fieldEntry.loadType());
+            if (fieldEntry.loadType() == DataTypes.BUFFER) {
+                if (fieldEntry.fieldId() == FRAGMENT) {
+                	System.out.println("=>FRAGMENT JSON zipped SIZE: "+fieldEntry.buffer().buffer().limit());//System.out.println("=>FRAGMENT JSON zipped SIZE: "+fieldEntry.buffer().byteBuffer().array().length);
+
+                    if(totalSize == 0) // not a first segment
+                        totalSize = totalSizes.get(guid).longValue();
+                    if (fieldEntry.buffer().buffer().limit() == totalSize) {//if (fieldEntry.buffer().byteBuffer().array().length == totalSize) {
+                        // there is only one segment, we are ready
+                        // unzip using gzip
+                    	String strFlatFrag = unzipPayload(Arrays.copyOf(fieldEntry.buffer().buffer().array(), fieldEntry.buffer().buffer().limit()));//String strFlatFrag = unzipPayload(fieldEntry.buffer().byteBuffer().array());
+                        System.out.println("=>FRAGMENT JSON STRING: " + strFlatFrag);
+                        try {
+                            JSONObject jsonResponse = new JSONObject(strFlatFrag);
+                            // pretty-print json response
+                            int spacesToIndentEachLevel = 2;
+                            System.out.println("FRAGMENT JSON PRETTY:\n" + jsonResponse.toString(spacesToIndentEachLevel));
+                             if(null != _mn) {
+                                int which = 0;
+                                if(null != closure)
+                                    which = ((Integer)closure).intValue();
+                                _mn.updateTxtArea(which, jsonResponse.toString(spacesToIndentEachLevel));
+                             } 
+                        } 
+                        catch (Exception e) {
+                            System.err.println("Exception parsing json: " + e);
+                            e.printStackTrace(System.err);
+                        }
+                    } else {
+                        ArrayList<ByteBuffer> alFrags;
+                        if(!fragBuilderHash.containsKey(guid)) {
+                            alFrags = new ArrayList<ByteBuffer>();
+                            alFrags.add(fieldEntry.buffer().buffer());//alFrags.add(fieldEntry.buffer().byteBuffer());
+                            fragBuilderHash.put(guid, alFrags);
+                        } else {
+                            alFrags = fragBuilderHash.get(guid);
+                            alFrags.add(fieldEntry.buffer().buffer());//alFrags.add(fieldEntry.buffer().byteBuffer());
+                            int sizeNow = 0;
+                            for(int i = 0; i < alFrags.size(); i++) {
+                            	sizeNow+=alFrags.get(i).limit();//sizeNow+=alFrags.get(i).array().length;
+                            }
+                            if(sizeNow < totalSize) {
+                                fragBuilderHash.put(guid, alFrags);
+                            } else {
+                                // we are ready
+                                String strFlatFrag = "";
+                                ByteArrayOutputStream fragOutputStream = new ByteArrayOutputStream( );
+
+                                for(int i = 0; i < alFrags.size(); i++) {
+                                    try {
+                                    	fragOutputStream.write( Arrays.copyOf(alFrags.get(i).array(), alFrags.get(i).limit()) );//fragOutputStream.write( alFrags.get(i).array() );
+                                    } catch(Exception e) {}
+                                }
+                                strFlatFrag+= unzipPayload(fragOutputStream.toByteArray());
+                                System.out.println("=>FRAGMENT JSON STRING: " + strFlatFrag);
+                        try {
+                            JSONObject jsonResponse = new JSONObject(strFlatFrag);
+                            // pretty-print json response
+                            int spacesToIndentEachLevel = 2;
+                            System.out.println("FRAGMENT JSON PRETTY:\n" + jsonResponse.toString(spacesToIndentEachLevel));
+                            /*                      if(mn.isInitCompleted()) {
+                             mn.updateTxtArea(0, jsonResponse.toString(spacesToIndentEachLevel));
+                             } else {
+                             //                         mn.updateTxtArea(0,"Update");
+                             }
+                             */                        } 
+                        catch (Exception e) {
+                            System.err.println("Exception parsing json: " + e);
+                            e.printStackTrace(System.err);
+                        }
+                            }
+                                    
+                        }
+                    }
+                }
+            } else if(fieldEntry.fieldId() == TOT_SIZE) {
+                totalSize = fieldEntry.uintValue();
+            } else if(fieldEntry.fieldId() == GUID) {
+                guid = fieldEntry.rmtes().toString();
+                if(totalSize!=0)
+                    totalSizes.put(guid, (new Long(totalSize)));
+            }
+        }
+    }
+
+    public String unzipPayload(byte[] bytes) {
+
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        try {
+            GZIPInputStream gis = new GZIPInputStream(bis);
+            byte[] buffer = new byte[bytes.length+1/*1024*/];
+            int len = -1;
+            while ((len = gis.read(buffer, 0, /*1024*/bytes.length+1)) != -1) {
+                bout.write(buffer, 0, len);
+            }
+        } catch (Exception e) {
+            System.out.println("Parse News error:" + e.getMessage());
+        }
+        return bout.toString();
+
+    }
+
+
+    void decode(Map map) {
+        System.out.println("Decode map:");
+        if (DataTypes.FIELD_LIST == map.summaryData().dataType()) {
+            System.out.println("Map Summary data:");
+            decode(map.summaryData().fieldList(),null);
+            System.out.println();
+        }
+
+        Iterator<MapEntry> iter = map.iterator();
+        MapEntry mapEntry;
+        while (iter.hasNext()) {
+            mapEntry = iter.next();
+
+            if (DataTypes.BUFFER == mapEntry.key().dataType()) {
+            	System.out.println("Action: " + mapEntry.mapActionAsString() + " key value: " + EmaUtility.asHexString(mapEntry.key().buffer().buffer()));//System.out.println("Action: " + mapEntry.mapActionAsString() + " key value: " + EmaUtility.asHexString(mapEntry.key().buffer().byteBuffer()));
+            }
+
+            if (DataTypes.FIELD_LIST == mapEntry.loadType()) {
+                System.out.println("Entry data:");
+                decode(mapEntry.fieldList(), null);
+                System.out.println();
+            }
+        }
+    }
+    
+}
+
+public class MRNConsumer implements Runnable{
+
+    protected static String _serviceName;
+    protected static String _ip;
+    protected static String _port;
+    protected static String _userName;
+    public static String _ricsMRN[]
+            = {"MRN_HDLN", "MRN_STORY", "MRN_TRSI", "MRN_TRNA"};
+    AppClient _appClient = null;
+    
+    public MRNConsumer(/*) {}
+    
+    public void mrnInit(*/String[] args) {
+        //  adslon01 14002 ERT_EDGE rmds
+        if (args == null || args.length != 4) {
+            System.out.println("Insufficient arguments - requires:");
+            System.out.println("serverip port servicename username");
+            System.out.println("e.g. 10.117.216.106 14002 ELEKTRON_DD testuser");
+            return;
+        }
+        
+        _ip = args[0];
+        _port = args[1];
+        _serviceName = args[2];
+        _userName = args[3];
+ 
+        try {
+            /*AppClient*/ _appClient = new AppClient();
+            OmmConsumerConfig config = EmaFactory.createOmmConsumerConfig();
+            OmmConsumer consumer = EmaFactory.createOmmConsumer(config.host(_ip + ":" + _port).username(_userName));
+            ReqMsg reqMsg = EmaFactory.createReqMsg();
+            for(int i = 0; i<_ricsMRN.length; i++) {
+                consumer.registerClient(reqMsg.domainType(EmaRdm.MMT_NEWS_TEXT_ANALYTICS).serviceName(
+                        _serviceName).name(_ricsMRN[i]), _appClient, (new Integer(i)));
+            }
+    //        Thread.sleep(60000);			// API calls onRefreshMsg(), onUpdateMsg() and onStatusMsg()		} catch ( OmmException excp ) {
+        } catch (OmmException excp) {
+            System.out.println(excp.getMessage());
+        }
+    }
+    
+    public void setFXMain(MRNFXMain mn){
+        _appClient.setFXMain(mn);
+    }
+    public void run() {
+        while(true) {
+            try {
+                Thread.sleep(100);           
+            } catch(InterruptedException ie){}
+        }
+    }
+}
---
+++ Thomson/src/com/thomsonreuters/ema/examples/mrn/MRNFXMain.java
@@ -0,0 +1,162 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thomsonreuters.ema.examples.mrn;
+
+import java.io.File;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author zoya.farberov
+ */
+public class MRNFXMain extends Application {
+    
+    private static int MAX_TEXTAREA_LEN = 125000;
+  //  private Object gridpane;
+    private TextArea txtAreas[];
+    private Button stopButtons[];
+    private boolean frozen[];
+    private MRNConsumer _consumer = null;
+    private static String[] _argsPassed;
+    private Thread _consumerThread = null;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        _consumer = new MRNConsumer(_argsPassed);
+        primaryStage.setTitle("MRN News Realtime");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        grid.getStyleClass().add("grid-pane");
+
+
+        ColumnConstraints columns[] = new ColumnConstraints[4];
+        for(int i = 0; i <4; i ++) {
+            columns[i] = new ColumnConstraints();
+            columns[i].setPercentWidth(25);
+        }
+        grid.getColumnConstraints().addAll(columns[0], columns[1], columns[2], columns[3]); 
+
+        txtAreas = new TextArea[4];
+        stopButtons = new Button[4];
+        frozen = new boolean[4];
+        for(int i = 0;i <4; i++) {
+            Label feedName = new Label(MRNConsumer._ricsMRN[i]);
+            txtAreas[i] = new TextArea();
+            txtAreas[i].setPrefRowCount(100);
+            txtAreas[i].setPrefColumnCount(100);
+            txtAreas[i].setWrapText(true);
+            txtAreas[i].setPrefWidth(150);
+            stopButtons[i] = new Button("Stop");
+            frozen[i] = false;
+            stopButtons[i].setOnAction(this::handleStopButtonAction);
+
+            GridPane.setHalignment(txtAreas[i], HPos.LEFT);
+            grid.add(feedName, i, 1);
+            grid.add(txtAreas[i], i, 3);
+            grid.add(stopButtons[i], i, 5);
+            String noDataYetTxt = "No data yet for feed "+feedName.getText()+"\n";        
+            txtAreas[i].appendText(noDataYetTxt);
+ //           txtAreas[i].getStyleClass().add("text-area");
+        }
+
+        Scene scene = new Scene(grid, 1200, 900);
+        System.out.println("scene stylesheets: " + scene.getStylesheets());
+
+        try {
+            File file = new File(".\\css\\dark.css");
+            URL url = file.toURI().toURL();
+            scene.getStylesheets().add(url.toExternalForm());
+            primaryStage.setScene(scene);        
+        } catch (Exception e) {
+            System.err.println("Exception reading css: "+ e);
+            e.printStackTrace(System.err);
+        }
+
+
+        primaryStage.show();
+        _consumerThread = new Thread(_consumer);
+        _consumer.setFXMain(this);
+        _consumerThread.start();  //      initCompleted = true;
+    }
+    
+    public void updateTxtArea(int i, String str) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                if (!frozen[i]) {
+                    String currentText = txtAreas[i].getText();
+                    if ((currentText.length() + str.length()) > MAX_TEXTAREA_LEN) {
+                        currentText = currentText.substring((new Double(MAX_TEXTAREA_LEN * 0.5)).intValue());
+                        txtAreas[i].clear();
+                        txtAreas[i].setText(currentText);
+                    }
+                    txtAreas[i].appendText(str);
+                }
+            }
+        });
+    }
+
+    private void handleStopButtonAction(ActionEvent event) {
+        for (int i = 0; i < 4; i++) {
+            if ((Button) event.getSource() == stopButtons[i]) {
+                if (frozen[i] == false) {
+                    frozen[i] = true;
+                    stopButtons[i].setText("Resume");
+                } else {
+                    frozen[i] = false;
+                    stopButtons[i].setText("Stop");
+                }
+            }
+        }
+    }
+
+    public void setConsumer(MRNConsumer consumer) {
+        _consumer = consumer;
+    }
+    
+    public MRNConsumer getConsumer() {
+        return _consumer;
+    }
+    
+    @Override
+    public void stop() {
+        System.exit(0);
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //    MRNConsumer cons = new MRNConsumer();
+    //     cons.mrnInit(mn, args);
+        MRNFXMain myMain = new MRNFXMain();
+        _argsPassed = new String[args.length];
+        for(int i = 0; i < args.length; i++)
+            _argsPassed[i] = args[i];
+        myMain.launch(args);
+    }
+    
+}
